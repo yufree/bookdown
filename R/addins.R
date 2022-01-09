482d6c62a6327982822fe309bd2f7be2e2e9c179
Yufree <yufree@live.cn>
2022-01-09 16:30:17 -0500

    add addins for image upload (copycat of blogdown)

--- R/addins.R
+++ R/addins.R
@@ -1,3 +1,15 @@
+# execute code in the site root dir
+pkg_file = function(..., mustWork = TRUE) {
+  system.file(..., package = 'bookdown', mustWork = mustWork)
+}
+
+in_root = function(expr) xfun::in_dir(getwd(), expr)
+
+source_addin = function(file) in_root(sys.source(
+  pkg_file('scripts', file), envir = new.env(parent = globalenv()),
+  keep.source = FALSE
+))
+
 jquery_dependency = function() {
   jquerylib::jquery_core()
 }
@@ -5,3 +17,5 @@ jquery_dependency = function() {
 mathquill = function() {
   sys.source(bookdown_file('scripts', 'mathquill.R'), new.env())
 }
+
+insert_image_addin = function() source_addin('insert_image.R')
